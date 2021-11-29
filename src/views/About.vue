f35875cd9af3d90a46c7c58ab6455535a3ebf061
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2021-11-29 15:22:52 +0000

    reloading to top

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,6 +10,14 @@ import aboutcompVue from "../components/aboutcomp.vue";
 import ctaVue from "../components/cta.vue";
 
 export default {
-  components:{aboutcompVue, ctaVue}
+  components:{aboutcompVue, ctaVue},
+
+  created() {
+    if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+// This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded. This has Cross-browser support.
+window.scrollTo(0,0);
+  },
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,13 @@ export default {
   components: {
     homepageVue,
     ctaVue
-  }
+  },
+   created() {
+    if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+// This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded. This has Cross-browser support.
+window.scrollTo(0,0);
+  },
 }
 </script>
