5c6cb10777edee76401d20debce73fa85d3d1c86
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-01-19 18:00:44 +0000

    products page

--- src/components/products.vue
+++ src/components/products.vue
@@ -22,11 +22,11 @@
         class="w-full lg:w-2/3 mx-auto rounded-xl overflow-hidden shadow border-1 border-gray-200 border-opacity-75 flex flex-col md:flex-row hover:shadow-lg transition-shadow ease-linear delay-200 cursor-pointer"
       >
         <div
-          class="w-full md:w-1/2 h-full overflow-hidden flex items-center bg-gray-100 p-5"
+          class="w-full md:w-1/2 h-full overflow-hidden flex items-center bg-gray-100 px-5 py-10"
         >
           <img src="../assets/@ Macbook Pro.png" class="my-auto w-full h-auto" alt="" />
         </div>
-        <div class="w-full md:w-1/2 p-5 md:p-10 bg-gray-100">
+        <div class="w-full md:w-1/2 px-5 py-0 md:py-5 md:p-10 bg-gray-100">
           <h4
             class="font-semibold tracking-tight leading-normal text-gray-900 md:mt-0 cursor-default"
             style="font-size: 24px"
@@ -46,7 +46,7 @@
             >
           </p>
           <button
-            class="text-white py-3 w-1/2 md:w-1/2 mt-5 text-base font-normal rounded-md"
+            class="text-white py-3 w-1/2 md:w-1/2 mt-5 mb-10 md:mb-0 text-base font-normal rounded-md"
             style="background-color: #b00000"
             @click="dutyCalc()"
             title="ghanadutycalculater.com"
