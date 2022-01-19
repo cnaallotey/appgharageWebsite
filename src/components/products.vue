c4857ba24efdce3a7e3c4688b197a93bacd9f195
Roberta Akoto <69573740+cnaallotey@users.noreply.github.com>
2022-01-19 17:53:46 +0000

    products page

--- src/components/products.vue
+++ src/components/products.vue
@@ -19,18 +19,14 @@
 
     <div class="space-y-10 md:space-y-0 md:grid md:gap-x-8 md:gap-y-10">
       <div
-        class="w-full lg:w-2/3 mx-auto rounded-xl overflow-hidden border-2 border-gray-100 border-opacity-50 flex flex-col md:flex-row hover:shadow-lg transition-shadow ease-linear delay-200 cursor-pointer"
+        class="w-full lg:w-2/3 mx-auto rounded-xl overflow-hidden shadow border-1 border-gray-200 border-opacity-75 flex flex-col md:flex-row hover:shadow-lg transition-shadow ease-linear delay-200 cursor-pointer"
       >
         <div
-          class="w-full md:w-1/2 h-full overflow-hidden flex items-center bg-gray-50 p-5"
+          class="w-full md:w-1/2 h-full overflow-hidden flex items-center bg-gray-100 p-5"
         >
-          <img
-            src="../assets/@ Macbook Pro.png"
-            class="my-auto w-full h-auto bg-gray-50"
-            alt=""
-          />
+          <img src="../assets/@ Macbook Pro.png" class="my-auto w-full h-auto" alt="" />
         </div>
-        <div class="w-full md:w-1/2 p-5 md:p-10 bg-gray-50">
+        <div class="w-full md:w-1/2 p-5 md:p-10 bg-gray-100">
           <h4
             class="font-semibold tracking-tight leading-normal text-gray-900 md:mt-0 cursor-default"
             style="font-size: 24px"
